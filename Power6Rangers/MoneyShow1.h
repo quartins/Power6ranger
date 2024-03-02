9bc0ba215acc653f01f350b75e46262a458c6185
xtxme <TT.icy013@gmail.com>
2024-03-03 00:39:15 +0700

    แก้บัคเสร็จล่ะจู้

--- Power6Rangers/MoneyShow1.h
+++ Power6Rangers/MoneyShow1.h
@@ -17,58 +17,8 @@ namespace Power6Rangers {
 	public:
 		MoneyShow1(void)
 		{
+
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
-
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~MoneyShow1()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	protected:
-	private: System::Windows::Forms::PictureBox^ pictureBox2;
-	private: System::Windows::Forms::Button^ button1;
-
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
-
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MoneyShow1::typeid));
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
-			this->SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Location = System::Drawing::Point(227, 105);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(226, 340);
-			this->pictureBox1->TabIndex = 0;
-			this->pictureBox1->TabStop = false;
-			this->pictureBox1->Click += gcnew System::EventHandler(this, &MoneyShow1::pictureBox1_Click);
-
 			int num;
 			Random^ random = gcnew Random;
 			num = random->Next(1, 14);
@@ -88,7 +38,7 @@ namespace Power6Rangers {
 				break;
 			case 7:this->pictureBox1->Load("07-pentacles.png");
 				break;
-			case 8:this->pictureBox1->Load("08-prntacles.png");
+			case 8:this->pictureBox1->Load("08-pentacles.png");
 				break;
 			case 9:this->pictureBox1->Load("09-pentacles.png");
 				break;
@@ -105,15 +55,6 @@ namespace Power6Rangers {
 
 			default: break;
 			}
-
-			// 
-			// pictureBox2
-			// 
-			this->pictureBox2->Location = System::Drawing::Point(719, 105);
-			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(226, 340);
-			this->pictureBox2->TabIndex = 1;
-			this->pictureBox2->TabStop = false;
 
 			if (num == 1)
 				this->pictureBox2->Load("show_pentacles01.jpg");
@@ -143,7 +84,69 @@ namespace Power6Rangers {
 				this->pictureBox2->Load("show_pentacles13.jpg");
 			else if (num == 14)
 				this->pictureBox2->Load("show_pentacles14.jpg");
+			//
+			//TODO: Add the constructor code here
+			//
+		}
 
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MoneyShow1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Button^ button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MoneyShow1::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox1->Location = System::Drawing::Point(227, 105);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(226, 340);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MoneyShow1::pictureBox1_Click);
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->pictureBox2->Location = System::Drawing::Point(719, 105);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(226, 340);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 1;
+			this->pictureBox2->TabStop = false;
 			// 
 			// button1
 			// 
@@ -170,6 +173,7 @@ namespace Power6Rangers {
 			this->Name = L"MoneyShow1";
 			this->Text = L"MoneyShow1";
 			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->Load += gcnew System::EventHandler(this, &MoneyShow1::MoneyShow1_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
@@ -181,5 +185,7 @@ namespace Power6Rangers {
 	}
 	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void MoneyShow1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
